70c9a3a6bfb2f92758f26ec7f07dca50d86c7a91
G-Losingson <georgesbyona@gmail.com>
2024-02-18 16:02:12 +0200

    suppression docs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("")
+    GMSServices.provideAPIKey("AIzaSyCe_uqjL8u2fxvhnxHNQ3u2a5PjVlKWcR4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
